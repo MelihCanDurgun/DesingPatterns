b326648141ed5543b8df7fa026cb58fe0ab3a0d7
Melih Can Durgun <melih2821@gmail.com>
2024-07-18 00:46:27 +0300

    10. video bitti

---
+++ DesingPattern.ChainofResponsibilty/ChainOfResponsibility/AreaDirector.cs
@@ -0,0 +1,34 @@
+﻿using DesingPattern.ChainofResponsibilty.DAL;
+using DesingPattern.ChainofResponsibilty.Models;
+
+namespace DesingPattern.ChainofResponsibilty.ChainOfResponsibility
+{
+    public class AreaDirector : Employee
+    {
+        public override void ProsessRequest(CustomerProcessViewModel req)
+        {
+            Context context = new Context();
+            if (req.Amount <= 400000)
+            {
+                CustomerProsses customerProsses = new CustomerProsses();
+                customerProsses.Amount = req.Amount.ToString();
+                customerProsses.Name = req.Name;
+                customerProsses.EmployeeName = "Bölge Direktörü - Zeynep Yılamz";
+                customerProsses.Description = "Para Çekme İşlemi Onaylandı, Müşteriye Talep Ettiği Tutar Ödendi";
+                context.CustomerProsesses.Add(customerProsses);
+                context.SaveChanges();
+            }
+            else if (NextApprover != null)
+            {
+                CustomerProsses customerProsses = new CustomerProsses();
+                customerProsses.Amount = req.Amount.ToString();
+                customerProsses.Name = req.Name;
+                customerProsses.EmployeeName = "Bölge Direktörü - Zeynep Yılamz";
+                customerProsses.Description = "Para Çekme Tutarı Bölge Direktörünün Günlük Ödeyebileceği Limiti Aştığı İçin İşlem Gerçekleştirilemedi , Müşterirnin Günlük Maksimum Çekebileceği Tutar 400.000 TL olup daha fazla için birden fazla şubeye gelmesi gerekli...";
+                context.CustomerProsesses.Add(customerProsses);
+                context.SaveChanges();
+                NextApprover.ProsessRequest(req);
+            }
+        }
+    }
+}
