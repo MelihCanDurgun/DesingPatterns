8e0190aff3b0f1ab8dadc463c822facbd87e6743
Melih Can Durgun <melih2821@gmail.com>
2024-07-18 00:33:50 +0300

    8.video bitti

---
+++ DesingPattern.ChainofResponsibilty/ChainOfResponsibility/ManagerAsistant.cs
@@ -0,0 +1,34 @@
+﻿using DesingPattern.ChainofResponsibilty.DAL;
+using DesingPattern.ChainofResponsibilty.Models;
+
+namespace DesingPattern.ChainofResponsibilty.ChainOfResponsibility
+{
+    public class ManagerAsistant : Employee
+    {
+        public override void ProsessRequest(CustomerProcessViewModel req)
+        {
+            Context context = new Context();
+            if(req.Amount<=150000)
+            {
+                CustomerProsses customerProsses = new CustomerProsses();
+                customerProsses.Amount = req.Amount.ToString();
+                customerProsses.Name = req.Name;
+                customerProsses.EmployeeName = "Şube Müdür Yardımcısı - Melike Öztürk";
+                customerProsses.Description = "Para Çekme İşlemi Onaylandı, Müşteriye Talep Ettiği Tutar Ödendi";
+                context.CustomerProsesses.Add(customerProsses);
+                context.SaveChanges();
+            }
+            else if (NextApprover != null)
+            {
+                CustomerProsses customerProsses = new CustomerProsses();
+                customerProsses.Amount = req.Amount.ToString();
+                customerProsses.Name = req.Name;
+                customerProsses.EmployeeName = "Şube Müdür Yardımcısı - Melike Öztürk";
+                customerProsses.Description = "Para Çekme Tutarı Şube Müdür Yardımcısının Günlük Ödeyebileceği Limiti Aştığı İçin İşlem Şube Müdürüne Yönlendirildii";
+                context.CustomerProsesses.Add(customerProsses);
+                context.SaveChanges();
+                NextApprover.ProsessRequest(req);
+            }   
+        }
+    }
+}
