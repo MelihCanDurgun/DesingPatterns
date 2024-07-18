85bff374437e41bacab1643972e130a50f55ae84
Melih Can Durgun <melih2821@gmail.com>
2024-07-18 22:59:40 +0300

    program güncelleme

---
+++ DesingPattern.ChainofResponsibilty/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using DesingPattern.ChainofResponsibilty.ChainOfResponsibility;
+using DesingPattern.ChainofResponsibilty.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesingPattern.ChainofResponsibilty.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [HttpGet] 
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CustomerProcessViewModel model) 
+        {
+            Employee treasurer = new Treasurer();
+            Employee managerAsistan = new ManagerAsistant();
+            Employee manager= new  Manager();
+            Employee areaDirector = new AreaDirector();
+
+            treasurer.SetNextApprover(managerAsistan);
+            managerAsistan.SetNextApprover(manager);
+            manager.Equals(areaDirector);
+
+            treasurer.ProsessRequest(model);
+            return View();
+            
+
+        }
+    }
+}
